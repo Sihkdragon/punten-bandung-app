1d3161a6803d2d5830d24561f987eb76b7519119
Sihkdragon <dithor37@gmail.com>
2022-11-19 13:24:04 +0700

    lot

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const app = require("./index");
 
-const PORT = 8080;
+const PORT = process.env.PORT | 8080;
 app.listen(PORT, (e) => {
   if (e) throw e;
 
+  console.clear();
   console.log(`---------------------------------------------------------`);
   console.log(`|\t⚡ [SERVER]: Running on port ${PORT}               |`);
   console.log(`|\t🌍 [SERVER]: http://localhost:${PORT}              |`);
