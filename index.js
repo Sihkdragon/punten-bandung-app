c923d7a43ca0007e0f549c025634334bfb1a854e
Sihkdragon <ditolord@gmail.com>
2023-03-20 23:29:56 +0700

    not using api route

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const router = require('./routes')
-const path = require('path')
-const dotenv = require('dotenv')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const router = require("./routes");
+const path = require("path");
+const dotenv = require("dotenv");
 
-const app = express()
+const app = express();
 
-dotenv.config()
-app.use(cors({ origin: '*' }))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'public')))
+dotenv.config();
+app.use(cors({ origin: "*" }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.json({ type: 'application/json' }))
-app.get('/', (req, res) => res.send('server is healthy'))
-app.use('/api', router)
+app.use(express.json({ type: "application/json" }));
+app.get("/", (req, res) => res.send("server is healthy"));
+// app.use('/api', router)
 
-module.exports = app
+module.exports = app;
