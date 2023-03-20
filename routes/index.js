1812d404db787b398f667ab5034edd0cfcf844a7
Sihkdragon <ditolord@gmail.com>
2023-03-20 23:44:38 +0700

    sts

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,4 @@
 const { Router } = require("express");
-const PostRouter = require("./PostRouter");
-const GalleryRouter = require("./GalleryRouter");
-const API_urls = require("../app/resources/urls");
-const AuthRouter = require("./AuthRouter");
-const CommentRouter = require("./CommentRouter");
-const UserRouter = require("./UserRouter");
 const V2Router = require("./v2");
 const router = Router();
 
@@ -12,29 +6,29 @@ const router = Router();
 // |           V2 URL                |
 // ================================================
 router.use("/v2", V2Router);
-// ================================================
-// |           Autehentication URL                |
-// ================================================
-router.use(API_urls.auth.url, AuthRouter);
+// // ================================================
+// // |           Autehentication URL                |
+// // ================================================
+// router.use(API_urls.auth.url, AuthRouter);
 
-// ================================================
-// |                Tabloid URL                   |
-// ================================================
-router.use(API_urls.post.url, PostRouter);
+// // ================================================
+// // |                Tabloid URL                   |
+// // ================================================
+// router.use(API_urls.post.url, PostRouter);
 
-// ================================================
-// |                Gallery URL                   |
-// ================================================
-router.use(API_urls.gallery.url, GalleryRouter);
+// // ================================================
+// // |                Gallery URL                   |
+// // ================================================
+// router.use(API_urls.gallery.url, GalleryRouter);
 
-// ================================================
-// |                Comments URL                  |
-// ================================================
-router.use(API_urls.comment.url, CommentRouter);
+// // ================================================
+// // |                Comments URL                  |
+// // ================================================
+// router.use(API_urls.comment.url, CommentRouter);
 
-// ================================================
-// |                Users URL                     |
-// ================================================
-router.use(API_urls.users.url, UserRouter);
+// // ================================================
+// // |                Users URL                     |
+// // ================================================
+// router.use(API_urls.users.url, UserRouter);
 
 module.exports = router;
